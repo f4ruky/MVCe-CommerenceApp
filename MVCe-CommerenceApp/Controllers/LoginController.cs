94db41acc539aa3c010904c927b4dd2764f83217
Muhammed Mirhan KAYA <mirhan.kaya@hotmail.com>
2023-05-01 22:04:34 +0300

    Login kısmı oluşturuldu. Cariler ile personellerin kullanım alanı ayrıldı. Cariler sayfasında profil bilgileri ve sipariş bilgileri eklendi

---
+++ MVCe-CommerenceApp/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCe_CommerenceApp.Models.Siniflar;
+using System.Web.Security;
+
+namespace MVCe_CommerenceApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return PartialView();
+        }
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler p)
+        {
+            
+            var bilgiler = c.Carilers.FirstOrDefault(x => x.CariMail == p.CariMail && x.CariSifre == p.CariSifre);
+            if( bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                
+                return RedirectToAction("Index","Login");
+            }
+            
+        }
+    }
+}
